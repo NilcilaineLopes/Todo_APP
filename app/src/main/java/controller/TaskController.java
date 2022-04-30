b22d37f0b3e297fda0bd3857dc9202273865c147
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-30 12:31:16 -0300

    Corrigi codigo e implementa metodo de update e list

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import model.Task;
 import util.ConnectionFactory;
 
@@ -16,7 +18,8 @@ import util.ConnectionFactory;
 public class TaskController {
     
     public void save(Task task){
-        String sql = "INSERT INTO tasks (idProject"
+        String sql = "INSERT INTO tasks ("
+                + "idProject"
                 + " name,"
                 + " description,"
                 + "completed,"
@@ -50,7 +53,35 @@ public class TaskController {
     }
     
     public void upadate(Task task){
-    
+        
+        String sql = "UPDATE tasks SET "
+                + "idProject = ?,"
+                + " name = ?,"
+                + " description = ?,"
+                + " completed = ?, "
+                + "notes = ?,"
+                + "deadline = ?,"
+                + "createAt = ?,"
+                + "update = ?"; 
+        
+        Connection connection = null;
+        PreparedStatement statement = null; 
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdatedAt().getTime()));          
+            statement.execute();           
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao atualizar a tarefa" + ex.getMessage(), ex);
+        }
     }    
     
     public void removeById(int taskId) throws SQLException{
@@ -65,13 +96,47 @@ public class TaskController {
             statement.setInt(1, taskId);
             statement.execute();
         }catch (Exception e) {
-            throw new RuntimeException("Erro ao deletar a");
+            throw new RuntimeException("Erro ao deletar a tarefa");
         } finally {
             ConnectionFactory.closeConnection(connection, statement);
         }
     }
     
     public List<Task> getAll(int idProject){
+        String sql =  "SELECT * FROM tasks WHERE idProject = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        
+        List<Task> tasks = new ArrayList<Task>();
+        
+        try{
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, idProject);
+            resultSet = statement.executeQuery();
+            
+            while (resultSet.next()){
+                
+                Task task = new Task();
+                task.setId(resultSet.getInt("id"));
+                task.setIdProject(resultSet.getInt("idProject"));
+                task.setName(resultSet.getString("name"));
+                task.setDescription(resultSet.getString("description"));
+                task.setIsCompleted(resultSet.getBoolean("completed"));
+                task.setNotes(resultSet.getString("notes"));
+                task.setDeadline(resultSet.getDate("deadline"));
+                task.setCreatedAt(resultSet.getDate("createdAt"));
+                task.setUpdatedAt(resultSet.getDate("updatedAt"));
+                
+                tasks.add(task);
+         }
+            
+        }catch(Exception ex){
+        
+        }
+        
         return null;
     }
     
